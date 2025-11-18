5ef778408800c5ba65c7a8556cc7a827b5afc632
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2025-11-18 15:38:19 +0100

    Updating runGame in Game.java

--- src/main/java/se/yrgo/game/ui/Game.java
+++ src/main/java/se/yrgo/game/ui/Game.java
@@ -1,21 +1,59 @@
 package se.yrgo.game.ui;
 
 import se.yrgo.game.Player;
+import se.yrgo.game.Room.Room;
+import se.yrgo.game.Room.RoomMain;
 import se.yrgo.game.monster.Monster;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Game {
+    public Player player;
+    public List<Room> rooms;
 
-    public static void runGame() {
-
+    public Game() {
+        initializeRooms();
     }
+
+    private void initializeRooms() {
+        rooms = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            rooms.add(RoomMain.getRoom());
+        }
+    }
+
+    public void runGame(Scanner scanner) {
+        System.out.println("Choose a player name: ");
+        String name = scanner.nextLine();
+
+        player = new Player(name, 100, 0);
+        System.out.printf("Welcome %s! You start your journey here...%n", player.getName());
+        System.out.println();
+
+        while (!rooms.isEmpty() || player.getHealth() <= 0) {
+            int roomCounter = 0;
+
+            encounterRoom(player);
+
+            rooms.remove(rooms.get(roomCounter));
+        }
+
+        if (player.getHealth() <= 0) {
+
+        }
+    }
+
 
     public void doBattle(Player player, Monster monster) {
         int damage = player.getAttackDamage();
         monster.wound(damage);
 
-        System.out.printf("You inflicted %d points of damage to the %s!%n", monster.getName());
+        System.out.printf("You inflicted %d points of damage to the %s!%n", damage, monster.getName());
         if (monster.getHealth() <= 0) {
-            System.out.printf( "You killed the %s!%n", monster.getName());
+            System.out.printf("You killed the %s!%n", monster.getName());
         }
 
         player.decreaseHealth(monster.getStrength());
