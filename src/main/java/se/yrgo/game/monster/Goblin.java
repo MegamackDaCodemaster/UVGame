a964ff313008b2c27fb324c276c4436330575225
Matilda Fahlström <matildafahlstrom90@hotmail.com>
2025-12-01 15:05:31 +0100

    Creating new monster class Goblin.java

---
+++ src/main/java/se/yrgo/game/monster/Goblin.java
@@ -0,0 +1,73 @@
+package se.yrgo.game.monster;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Goblin implements Monster{
+    private final String monsterName;
+    private final String description;
+    private int health;
+    private final int rewardPoints;
+    private final int strength;
+    private final Random random;
+
+    /**
+     * Constructs a default {@code Goblin} with pre-defined stats.
+     */
+    public Goblin() {
+        this("Goblin", "a sneaky goblin", 10, 10, 15);
+    }
+
+    public Goblin(String monsterName, String description, int rewardPoints, int health, int strength) {
+        Objects.requireNonNull(monsterName, "The parameter 'monsterName' is required for this constructor");
+        Objects.requireNonNull(description, "The parameter 'description' is required for this constructor");
+
+        if (health <= 0) {
+            throw new IllegalArgumentException("The health value need to be more than 0.");
+        }
+
+        if (strength <= 0) {
+            throw new IllegalArgumentException("The strength value need to be more than 0.");
+        }
+
+        this.monsterName = monsterName;
+        this.description = description;
+        this.rewardPoints = rewardPoints;
+        this.health = health;
+        this.strength = strength;
+        this.random = new Random();
+    }
+
+
+    @Override
+    public String getMonsterName() {
+        return monsterName;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public int getRewardPoints() {
+        return rewardPoints;
+    }
+
+    @Override
+    public int getHealth() {
+        return health;
+    }
+
+    @Override
+    public int getStrength() {
+        return strength;
+    }
+
+    @Override
+    public void wound(int hitPoints) {
+        if (health > 0 && hitPoints > 0) {
+            health -= hitPoints;
+        }
+    }
+}
